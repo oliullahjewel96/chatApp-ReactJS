ba25125b321325d126828c82292ec0d0d098f3e2
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-12-23 00:01:43 +0600

    Added logic for my messages

--- chat-app/src/components/MyMessage.jsx
+++ chat-app/src/components/MyMessage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-function MyMessage() {
-  return <div>MyMessage</div>;
+function MyMessage({ message }) {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachment[0].file}
+        alt="message-attachment"
+        style={{ float: "right" }}
+      />
+    );
+  }
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "#fff",
+        backgroundColor: "#3B5A20",
+      }}
+    >
+      {message.text}
+    </div>
+  );
 }
 
 export default MyMessage;
