164bc6346dad3cf66164b95f21895f8ef58932ab
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-12-22 17:13:49 +0600

    initial commit for the Chat Application Project

---
+++ chat-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <ChatEngine />
+    </div>
+  );
+};
+
+export default App;
