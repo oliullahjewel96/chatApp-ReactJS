57fb0de30dfe12506f10dd1bb6b50ebfa1b09c69
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-12-22 23:06:37 +0600

    Added different components for chatfeed

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <ChatEngine />
+      <ChatEngine
+        height="100vh"
+        projectID="8c2c6fbb-e8ea-456c-9ac8-43d1c0df36af"
+        userName="Kop10"
+        userSecret="123456"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
     </div>
   );
 };
---
+++ chat-app/src/components/ChatFeed.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function ChatFeed(props) {
+  console.log(props);
+  return <div>ChatFeed</div>;
+}
